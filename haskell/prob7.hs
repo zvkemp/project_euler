00f2092a6d181001fbb01982765628a158fb76b4
zvkemp <zvkemp@gmail.com>
2014-04-22 11:29:33 -0700

    haskell problem 7

---
+++ haskell/prob7.hs
@@ -0,0 +1,16 @@
+--What is the 10001st prime number?
+
+-- all primes other than 2 or 3 are of the form 6k + 1 or 6k + 5
+-- the list comprehension is the set of prime candidates
+primes = filter (is_prime) (2:3:[6*k + r | k <- [0..], r <- [1, 5]])
+
+is_prime :: Int -> Bool
+is_prime 0 = False
+is_prime 1 = False
+is_prime 2 = True
+is_prime n
+  | any (\y -> (mod n y) == 0) pr = False
+  | otherwise                     = True
+  where pr = takeWhile (\x -> x*x <= n) primes
+
+main = print (primes !! 10000)
